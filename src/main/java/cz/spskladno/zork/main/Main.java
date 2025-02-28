a4bd886db2b1292f21630013c2c7ba1e9b38c17e
RobaCZ <robachannel@gmail.com>
2025-02-28 21:02:02 +0100

    Refactor Main and build.gradle

--- src/main/java/cz/spskladno/zork/main/Main.java
+++ src/main/java/cz/spskladno/zork/main/Main.java
@@ -9,10 +9,7 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
-        // Set encoding to UTF-8
         System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, StandardCharsets.UTF_8));
-
-        // Start CommandLineUI
         CommandLineUI.getInstance().start();
     }
 }
