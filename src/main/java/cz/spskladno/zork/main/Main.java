e2df09a440d605a22eb584195e3b7036ac740469
RobaCZ <robachannel@gmail.com>
2025-02-28 20:49:44 +0100

    Fix Charset to UTF-8

--- src/main/java/cz/spskladno/zork/main/Main.java
+++ src/main/java/cz/spskladno/zork/main/Main.java
@@ -2,8 +2,17 @@ package cz.spskladno.zork.main;
 
 import cz.spskladno.zork.ui.CommandLineUI;
 
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
     public static void main(String[] args) {
+        // Set encoding to UTF-8
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, StandardCharsets.UTF_8));
+
+        // Start CommandLineUI
         CommandLineUI.getInstance().start();
     }
 }
